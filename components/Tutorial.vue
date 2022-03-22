e505b8d2ff411fb95bd46aa252aa6942f5f290c5
Jeff Holst <jeffholst@gmail.com>
2022-03-21 22:11:57 -0500

    Blast off!

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Blast off!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
